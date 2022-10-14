fe22426c7f3377ca01228069eef79375fe6a43c1
jasonLapina <lapina.jason@gmail.com>
2022-10-14 11:29:05 +0800

    finish layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.scss';
+
+const PlayerCard = (props) => {
+  return (
+    <div className={`${classes.player} ${classes[`player--${props.player}`]}`}>
+      <div>
+        Player: {props.player}
+        <span>0</span>
+      </div>
+      <div className={classes.current}>
+        current: <span>0</span>
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={classes.container}>
+      <button className={classes.reset}>new game</button>
+      <PlayerCard player={1} />
+      <PlayerCard player={2} />
+      <div className={classes.buttons}>
+        <button className={classes.roll}>🎲 Roll dice</button>
+        <button className={classes.hold}>📥 Hold</button>
+      </div>
+    </main>
   );
 }
 
